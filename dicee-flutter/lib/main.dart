efb7fe89a80c22ee40bc22f6dd8fdd38fc72d725
yasmin salah <50454254+YasminSalahEldin@users.noreply.github.com>
2020-05-16 10:36:04 -0700

    Our Goal
    The objective of this tutorial is to introduce you to the core programming concepts that will form the foundation of most of the apps you’ll build in the future. This app will teach you how to make apps with functionality using setState() inside Stateful Flutter widgets.

---
+++ dicee-flutter/lib/main.dart
@@ -0,0 +1,66 @@
+
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDiceFace(){
+    setState(() {
+      rightDiceNumber = Random().nextInt(6)+1;
+      leftDiceNumber = Random().nextInt(6)+1;
+
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Center(
+      child: Row (
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                changeDiceFace();
+              },
+                child: Image.asset('images/dice$leftDiceNumber.png')
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                changeDiceFace();
+              },
+                child: Image.asset('images/dice$rightDiceNumber.png')
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
